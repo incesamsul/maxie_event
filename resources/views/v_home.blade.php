ae6948e015663e649ee778a1f2fa33ffc057cd11
incesamsul <incesamsul89@gmail.com>
2023-02-10 20:31:20 +0800

    fix date in home pages

--- resources/views/v_home.blade.php
+++ resources/views/v_home.blade.php
@@ -72,7 +72,7 @@
         <div class="row no-gutters slider-text  js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
           <div class="col-xl-10 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
             <h1 class="mb-4 " data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">  Reward <br><span>Maxie skincare</span></h1>
-            <p class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Februari 12, 2023. Makassar, Indonesia</p>
+            <p class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Februari 13, 2023. Makassar, Indonesia</p>
             <div id="timer" class="d-flex mb-3">
 						  <div class="time" id="days"></div>
 						  <div class="time pl-4" id="hours"></div>
